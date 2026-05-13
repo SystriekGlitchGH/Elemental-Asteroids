26ab7c355318638679d00cd0315da0d484bf8470
Arcturus <systriekglitchyt@gmail.com>
2026-05-13 10:19:25 -0700

    started on movement

---
+++ Assets/Scripts/Monoscripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rb2d;
+    [Header("Movement")]
+    public float acceleration;
+    public float maxSpeed;
+    public float friction;
+    private float directionX, directionY;
+    private void Update()
+    {
+        // adding acceleration to the directions
+        Vector2 newVelocity = new Vector2(directionX * acceleration, directionY * acceleration);
+        // adding the force to the rigidbody2d
+        rb2d.AddForce(newVelocity * Time.deltaTime);
+        // maxing the velocity of the player to the playerStats.baseSpeed variable
+        Vector2 velocity = Vector2.ClampMagnitude(new Vector2(rb2d.linearVelocity.x, rb2d.linearVelocity.y), maxSpeed);
+        // applying clamped velocity to rigidbody2d
+        rb2d.linearVelocity = velocity;
+        // when both direction inputs are 0, add linear damping to slow down player quickly
+        if (directionX == 0 && directionY == 0) rb2d.linearDamping = friction;
+        else rb2d.linearDamping = 0;
+    }
+    //private void FixedUpdate()
+    //{
+        
+    //}
+    public void Move(InputAction.CallbackContext ctx)
+    {
+        directionX = ctx.ReadValue<Vector2>().x;
+        directionY = ctx.ReadValue<Vector2>().y;
+    }
+}
