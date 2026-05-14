f87b5881d5d3a3de63f01b4f42c159d7cf6c9819
Arcturus <systriekglitchyt@gmail.com>
2026-05-13 22:49:22 -0700

    started on gravity sources

--- Assets/Scripts/Monoscripts/PlayerMovement.cs
+++ Assets/Scripts/Monoscripts/PlayerMovement.cs
@@ -3,18 +3,23 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Components")]
     public Rigidbody2D rb2d;
+    public Transform gravitySource;
     [Header("Movement")]
     public float acceleration;
     public float maxSpeed;
     public float friction;
     private float directionX, directionY;
-    private void Update()
+    private void Update(){
+        
+    }
+    private void FixedUpdate()
     {
         // adding acceleration to the directions
         Vector2 newVelocity = new Vector2(directionX * acceleration, directionY * acceleration);
         // adding the force to the rigidbody2d
-        rb2d.AddForce(newVelocity * Time.deltaTime);
+        rb2d.AddRelativeForce(newVelocity);
         // maxing the velocity of the player to the playerStats.baseSpeed variable
         Vector2 velocity = Vector2.ClampMagnitude(new Vector2(rb2d.linearVelocity.x, rb2d.linearVelocity.y), maxSpeed);
         // applying clamped velocity to rigidbody2d
@@ -23,10 +28,6 @@ public class PlayerMovement : MonoBehaviour
         if (directionX == 0 && directionY == 0) rb2d.linearDamping = friction;
         else rb2d.linearDamping = 0;
     }
-    //private void FixedUpdate()
-    //{
-        
-    //}
     public void Move(InputAction.CallbackContext ctx)
     {
         directionX = ctx.ReadValue<Vector2>().x;
