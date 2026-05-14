4a373a2e5e8e147f61ae3f2b0c84fc0a2865e24b
Arcturus <systriekglitchyt@gmail.com>
2026-05-14 10:20:27 -0700

    added orbit

--- Assets/Scripts/Monoscripts/GravitySource.cs
+++ Assets/Scripts/Monoscripts/GravitySource.cs
@@ -1,6 +1,27 @@
+using System;
 using UnityEngine;
 
 public class GravitySource : MonoBehaviour
 {
-    
+    [SerializeField] PlayerMovement pm;
+
+    public float AtmosphericDrag;
+    [SerializeField] int mass;
+    public void FixedUpdate()
+    {
+        if (pm != null)
+        {
+            Vector2 direction = (transform.position - pm.transform.position).normalized;
+            float scale = (float)(((pm.rb2d.mass * mass) / Math.Pow(Vector2.Distance(pm.transform.position, transform.position), 2))) * 100;
+            pm.rb2d.AddForce(direction * scale);
+        }
+    }
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(TryGetComponent(out PlayerMovement foundPlayer))
+        {
+            pm = foundPlayer;
+            pm.rb2d.linearDamping = AtmosphericDrag;
+        }
+    }
 }
--- Assets/Scripts/Monoscripts/PlayerMovement.cs
+++ Assets/Scripts/Monoscripts/PlayerMovement.cs
@@ -5,25 +5,23 @@ public class PlayerMovement : MonoBehaviour
 {
     [Header("Components")]
     public Rigidbody2D rb2d;
-    public Transform gravitySource;
     [Header("Movement")]
     public float acceleration;
+    public float topMovementSpeed;
     public float maxSpeed;
     public float friction;
     private float directionX, directionY;
     private void FixedUpdate()
     {
-        // adding acceleration to the directions
-        Vector2 newVelocity = new Vector2(directionX * acceleration, directionY * acceleration);
-        // adding the force to the rigidbody2d
-        rb2d.AddRelativeForce(newVelocity);
-        // maxing the velocity of the player to the playerStats.baseSpeed variable
-        Vector2 velocity = Vector2.ClampMagnitude(new Vector2(rb2d.linearVelocity.x, rb2d.linearVelocity.y), maxSpeed);
-        // applying clamped velocity to rigidbody2d
-        rb2d.linearVelocity = velocity;
-        // when both direction inputs are 0, add linear damping to slow down player quickly
-        if (directionX == 0 && directionY == 0) rb2d.linearDamping = friction;
-        else rb2d.linearDamping = 0;
+        // velocity based on accelteration and direction
+        Vector2 movementVelocity = new Vector2(directionX * acceleration, directionY * acceleration);
+        rb2d.AddRelativeForce(movementVelocity);
+        if (movementVelocity.magnitude > topMovementSpeed)
+            movementVelocity = movementVelocity.normalized * topMovementSpeed;
+        Vector2 externalVelocity = rb2d.linearVelocity - movementVelocity;
+        rb2d.linearVelocity = movementVelocity + externalVelocity;
+        rb2d.linearVelocity = Vector2.ClampMagnitude(rb2d.linearVelocity,maxSpeed);
+        Debug.Log(rb2d.linearVelocity.magnitude);
     }
     public void Move(InputAction.CallbackContext ctx)
     {
